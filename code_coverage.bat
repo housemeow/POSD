6f9d647980b407cfd462ab03b480249d67d12d32
Kai <housemeow@gmail.com>
2014-11-20 13:29:20 +0800

    code coverage bat

--- code_coverage.bat
+++ code_coverage.bat
@@ -1,3 +1,30 @@
 rmdir CppCoverageReport-ProductionCode /s /q
 OpenCppCoverage.exe --source 103598010_MindMap --output CppCoverageReport-ProductionCode --excluded_sources 103598010_Test -- Debug\103598010_Test.exe 
 start CppCoverageReport-ProductionCode/index.html
+
+
+rmdir CppCoverageReport-TestCode /s /q
+OpenCppCoverage.exe --source MindMapTest --output CppCoverageReport-TestCode -- Debug\MindMapTest.exe
+start CppCoverageReport-TestCode/index.html
+
+
+rmdir CppCoverageReport-ProductionCode /s /q
+OpenCppCoverage.exe --source mindmap --output CppCoverageReport-ProductionCode --excluded_sources mindmaptest -- Debug\MindMapTest.exe 
+start CppCoverageReport-ProductionCode/index.html
+
+
+
+
+
+
+rmdir CppCoverageReport-TestCode /s /q
+OpenCppCoverage.exe --source 103598010_Test --output CppCoverageReport-TestCode -- Debug\103598010_Test.exe
+start CppCoverageReport-TestCode/index.html
+
+
+rmdir CppCoverageReport-ProductionCode /s /q
+OpenCppCoverage.exe --source 103598010_MindMap --output CppCoverageReport-ProductionCode --excluded_sources 103598010_Test -- Debug\103598010_Test.exe 
+start CppCoverageReport-ProductionCode/index.html
+
+
+
